20194387a7383abb64946789e148b7c06991253b
Ahazeez <abdulhazeezkepu@gmail.com>
2026-04-17 18:07:42 +0530

    the programming

---
+++ last_moving_elements.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class moving_elements {
+    public static void main(String[] args) {
+        int a[] = {10, 0, 20, 0, 50, 80};
+            move(a);
+        
+            System.out.print(Arrays.toString(a));
+        
+    }
+
+    public static void move(int[] arr) {
+        // Implementation to move elements
+        int count = 0; // Count of non-zero elements
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] != 0) {
+                arr[count--] = arr[i]; // Move non-zero element to the front
+            }
+        }
+        // Fill the remaining positions with zeros
+        while (count < arr.length) {
+            arr[count++] = 0;
+        }
+
+
+    }
+
+}
